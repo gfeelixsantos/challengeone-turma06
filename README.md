e34c8055b7cb855987569c6cb003bb02edea3296
gfeelixsantos <felix.devx@gmail.com>
2024-03-10 20:22:33 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Projeto Challenge ONE - Oracle Next Education
+
+*Este projeto faz parte do desafio Challenge One em parceria com Alura na formação de novos desenvolvedores durante o ano de 2024.*
+
+<hr>
+
+### Objetivo
+*Tivemos como desafio desenvolver um encriptador de texto com a capacidade de codificar e descodificar  mensagens.*
+
+<hr>
+
+### Preview
+
+<img src="https://i.ibb.co/rvVksvG/preview.gif" alt="preview" border="0">
+
+#### Confira a página completa através do GitHub Pages.
+
+<hr>
+
+### Tecnologias
+<a href="https://developer.mozilla.org/pt-BR/docs/Web/HTML" target="_blank">
+    <img src="https://cdn-icons-png.flaticon.com/128/5968/5968267.png" width="50">
+</a>
+<a href="https://developer.mozilla.org/pt-BR/docs/Web/CSS" target="_blank">
+    <img src="https://cdn-icons-png.flaticon.com/128/5968/5968242.png" width="50">
+</a>
+<a href="https://developer.mozilla.org/pt-BR/docs/Web/JavaScript" target="_blank">
+    <img src="https://cdn-icons-png.flaticon.com/128/5968/5968292.png" width="50">
+</a>
---
+++ src/js/criptografar.js
@@ -0,0 +1,67 @@
+const textArea = document.querySelector('#texto')
+const btn_criptografar = document.querySelector('#criptografar')
+const btn_descriptografar = document.querySelector('#descriptografar')
+const btn_copiar = document.querySelector('body > main > div:nth-child(2) > div > button')
+
+const imgSemMensagem = document.querySelector('body > main > div:nth-child(2) > div > img')
+const textAreaResultado = document.querySelector('body > main > div:nth-child(2) > div > textarea')
+const textoTitulo = document.querySelector('body > main > div:nth-child(2) > div > h4')
+
+btn_criptografar.addEventListener('click', criptografar)
+btn_descriptografar.addEventListener('click', descriptografar)
+btn_copiar.addEventListener('click', copiarTexto)
+
+const criptoA = 'uzhaf'
+const criptoE = '£on'
+const criptoI = 'lwher'
+const criptoO = 'gdhep'
+const criptoU = 'kveq'
+
+function criptografar() {
+
+    let texto = ''
+    
+    for( letra of textArea.value ){
+        
+        if( letra == 'a' || letra == 'A' ){ letra = criptoA }
+        if( letra == 'e' || letra == 'E' ){ letra = criptoE }
+        if( letra == 'i' || letra == 'I' ){ letra = criptoI }
+        if( letra == 'o' || letra == 'O' ){ letra = criptoO }
+        if( letra == 'u' || letra == 'U' ){ letra = criptoU }
+
+        texto += letra
+
+    }
+    
+    textoTitulo.innerHTML = 'Criptografado com sucesso!'
+    textAreaResultado.value = texto
+
+    imgSemMensagem.remove()
+    textAreaResultado.removeAttribute('hidden')
+    btn_copiar.removeAttribute('hidden')
+
+}
+
+function descriptografar() {
+
+    let texto = textArea.value
+    texto = texto.replaceAll(criptoA, 'a')
+    texto = texto.replaceAll(criptoE, 'e')
+    texto = texto.replaceAll(criptoI, 'i')
+    texto = texto.replaceAll(criptoO, 'o')
+    texto = texto.replaceAll(criptoU, 'u')
+    console.log(texto);
+    
+    textoTitulo.innerHTML = 'Descriptografado com sucesso!'
+    textAreaResultado.value = texto
+
+    imgSemMensagem.remove()
+    textAreaResultado.removeAttribute('hidden')
+    btn_copiar.removeAttribute('hidden')
+
+}
+
+function copiarTexto(){
+    navigator.clipboard.writeText(textAreaResultado.value)
+    alert('A mensagem foi copiada para sua área de transferência')
+}
